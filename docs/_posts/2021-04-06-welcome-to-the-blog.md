b47b574604efc6a948151b4d28a35bdcd4ec4140
Undercover Stamp <byemc@outlook.com>
2021-04-06 21:02:21 +0100

    lots of shit

---
+++ docs/_posts/2021-04-06-welcome-to-the-blog.md
@@ -0,0 +1,9 @@
+---
+layout: post
+author: ByeMC
+tags: jekyll github-pages html
+categories: technology websites
+---
+
+So I'm learning how to use [Jekyll](https://jekyllrb.com/), and it's *fucking amazing*. <!--more--> So, I made this [GitHub Repository](https://github.com/ByeMC/blog) to make a blog with Jekyll.
+
---
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome To The Blog",
+        "excerpt":"So I’m learning how to use Jekyll, and it’s fucking amazing.  So, I made this GitHub Repository to make a blog with Jekyll.   ","categories": ["technology","websites"],
+        "tags": ["jekyll","github-pages","html"],
+        "url": "/technology/websites/2021/04/06/welcome-to-the-blog.html",
+        "teaser": null
+      }]
